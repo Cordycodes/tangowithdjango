2080fd4b3f14958e27b2d17d74635fdd041895e3
unknown <2038363s@BO706-39.ad.dcs.gla.ac.uk>
2015-01-22 13:50:42 +0000

    Chapter 4 Extra Exercises Completed and Picture Added

--- firsttango/rango/views.py
+++ firsttango/rango/views.py
@@ -3,5 +3,11 @@ from django.shortcuts import render
 from django.http import HttpResponse	
 
 def index(request):
-	return HttpResponse("Rango says fuck you, world!")
+	return HttpResponse("Rango says I love you, world! \
+                        <br> <a href ='/rango/about'> About</a>")
+
+def about(request):
+    return HttpResponse("This page brought to you by Cordelia Schmid (2038363s)."\
+                        "<br> <img src='http://i.imgur.com/NzHBDjO.jpg' style='width:350px;height:467px'>"\
+                        "<br> Back to index:<a href='/rango/'> Index</a>")
 	
