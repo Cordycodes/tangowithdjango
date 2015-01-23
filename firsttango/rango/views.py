e66b777c65fbcc714d6c06cd6ab059c7de167ae9
schmid.cordelia@gmail.com <2038363s@BO706-03.ad.dcs.gla.ac.uk>
2015-01-23 10:31:09 +0000

    Chapter 6 Exercises Completed

--- firsttango/rango/views.py
+++ firsttango/rango/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from rango.models import Category
 
 from django.http import HttpResponse	
 
 def index(request):
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'boldmessage': "I am big, black and beautiful"}
     return render(request, 'rango/index.html', context_dict)
 
